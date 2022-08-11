20042d14d893b09c97a89bf505ee5818be506964
uxoadev <dev.uxoa@gmai.com>
2022-08-11 02:03:52 -0300

    home atualizada

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,15 +1,22 @@
 import React from "react"
+import "./styles.css"
+import inicial1 from "../../imagens/inicial1.png"
 
-function Home(){
-    return(
-        <div>
-            
-            <h1>PAGINA HOME</h1>
-
-            
+function Home() {
+    return (
+        <div className="home">
+            <div>
+                <h2>CUIDO DOS SEUS DIREITOS</h2>
+                <h1>HILQUIAS JORDÃO</h1>
+                <h4>Advocacia e assessoria jurídica</h4>
+                <h4>Eficiência, transparência e atendimento individualizado na sua demanda.</h4>
+            </div>
+            <img src={inicial1} alt="" />
+           
         </div>
         
-        
+
+
     )
 
 } export default Home
